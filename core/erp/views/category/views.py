70408385be7fb807c2e1dff961c226f8240266f9
Harinson Arellan <harrinson.arellan@gmail.com>
2022-01-16 20:35:58 -0300

    video 21

--- core/erp/views/category/views.py
+++ core/erp/views/category/views.py
@@ -1,6 +1,9 @@
+from typing import Any, Dict
+from django.db.models.query import QuerySet
 from django.shortcuts import render
+from django.views.generic import ListView
 
-from core.erp.models import Category
+from core.erp.models import Category, Product
 
 def category_list(request):
     data = {
@@ -8,3 +11,17 @@ def category_list(request):
         'category': Category.objects.all()
     }
     return render(request, 'category/list.html', data)
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'category/list.html'
+
+    # def get_queryset(self):
+    #     return Product.objects.all()
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        print(context)
+        context['title'] = 'Listado de Categorías'
+        #context['object_list'] = Product.objects.all()
+        return context
